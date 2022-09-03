72e3f1d0ec0c9a2e64320fa1d20de6811a96e372
glynk <glyn_knight@hotmail.com>
2022-09-03 20:20:08 +0100

    region filter working but one behind

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,24 +1,54 @@
 import List from "../List/list"
 import SearchBar from '../Search Bar/searchBar';
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 
 
 export default function Home(){
 
     const [searchState, setSearchState] = useState('')
     const [regionState, setRegionState] = useState('All')
+    const [countries, setCountries] = useState([])
+    const [displayData, setDisplayData]= useState([])
+
+    useEffect(() => {
+
+        const fetchCountryData = async() => {
+            const response = await fetch("https://restcountries.com/v3.1/all")
+            const fetchCountries = await response.json()
+            setCountries(fetchCountries)
+            setDisplayData(fetchCountries)
+                        console.log(fetchCountries)
+        }
+        fetchCountryData()  
     
+    }, [])
     
     function onInput(e){
       setSearchState(e.target.value)
       console.log(searchState)
+      
     }
     
     function onSelect(e){
       setRegionState(e.target.value)
       e.preventDefault()
       console.log(regionState)
+        let regionData = []
+      for (let i = 0; i<countries.length;i++){
+          if(regionState === countries[i].region){
+              regionData.push(countries[i])
+              
+          }
+          
+      }
+      setDisplayData(regionData)
+      console.log(displayData)
     }
+    
+    
+    
+     
+    
     
     
 
@@ -29,7 +59,7 @@ return<div className="App">
   </header>
   <div className="content-display">
   <SearchBar onInput={onInput} onSelect={onSelect} />
-  <List name={searchState} region={regionState}/>
+  <List countries={displayData} name={searchState} region={regionState}/>
   </div>
 </div>
 
--- src/Components/List/list.js
+++ src/Components/List/list.js
@@ -1,77 +1,16 @@
-import React from "react";
-import  { useState, useEffect} from "react";
 import Card from "../Card/card";
-
 import {Link} from "react-router-dom";
 
 
-const List = ({ name, region }) => {
-    const [countries, setCountries] = useState([])
+const List = ({ name, region, countries }) => {
+   
+
+
+
+
+
+
     
-
-    /*
-    When user types into the search bar and submits, matching results should be returned.
-
-    When user selects region, matching results should be returned.
-    */
-
-
-    useEffect(() => {
-
-        const fetchCountryData = async() => {
-            const response = await fetch("https://restcountries.com/v3.1/all")
-            const fetchCountries = await response.json()
-            setCountries(fetchCountries)
-                        console.log(fetchCountries)
-        }
-        fetchCountryData()  
-    }, [])
-
-
-    /*
-    useEffect(()=>{
-        const fetchSearch = async() => {
-            const response = await fetch(`https://restcountries.com/v3.1/name/${name}`)
-            const fetchCountries = await response.json()
-           setCountries(fetchCountries)
-              console.log(fetchCountries)
-        }
-        
-        fetchSearch()
-    }, [name])
-        */
-       
-        // const fetchRegion = async() => {
-        //     const response = await fetch(`https://restcountries.com/v3.1/region/${region}`)
-        //     const fetchCountries = await response.json()
-        //     setCountries(fetchCountries)
-        //     console.log(fetchCountries)
-        // }
-     /*   
-    let filteredByRegion = []
-        if(name === '') {
-                for(let i = 0; i < countries.length; i++) {
-                if(countries[i].region === region){
-                    filteredByRegion.push(countries[i])
-                }
-            }
-            setCountries(filteredByRegion)
-            console.log('Filtered', filteredByRegion)
-            console.log('Region', region)
-        } else {
-          
-            for(let i = 0; i < countries.length; i++) {
-                if(countries[i].region === region){
-                    filteredByRegion.push(countries[i])
-                }
-            } 
-            setCountries(filteredByRegion)
-            console.log('Filtered', filteredByRegion)
-            console.log('Region', region)
-        }
-
-*/
-        
   
     return<div className="list">
         {countries.map((country) => <Link to={`/country/${country.name.common}`} state={{ from: {country} }} style={{ textDecoration: 'none', color: 'black'}}><Card
